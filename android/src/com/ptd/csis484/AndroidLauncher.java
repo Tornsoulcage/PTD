74f394a2769fcd4a41087e316d92f8132500642f
Tornsoulcage <scottjoseph96@gmailcom>
2018-02-18 17:35:55 -0600

    Created Project

---
+++ android/src/com/ptd/csis484/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ptd.csis484;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ptd.csis484.PTD;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PTD(), config);
+	}
+}
---
+++ core/src/com/ptd/csis484/GameScreen.java
@@ -0,0 +1,73 @@
+package com.ptd.csis484;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+
+
+/**
+ * Created by scott on 2/18/2018.
+ */
+
+public class GameScreen implements Screen {
+    final PTD game;
+    OrthographicCamera camera;
+    TiledMap tiledMap;
+    TiledMapRenderer tiledMapRenderer;
+
+    public GameScreen(final PTD game){
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 480, 320);
+        camera.update();
+
+        tiledMap = new TmxMapLoader().load("assets/PTDMap.tmx");
+        tiledMapRenderer = new OrthoCachedTiledMapRenderer(tiledMap);
+    }
+
+    @Override
+    public void render(float delta){
+        Gdx.gl.glClearColor(0,0,0.2f,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        tiledMap.dispose();
+    }
+}
