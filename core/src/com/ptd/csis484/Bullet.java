892e7f6798a275cc2bd8625b8f5b7dea181f14a4
Tornsoulcage <scottjoseph96@gmailcom>
2018-03-05 11:38:57 -0600

    Bulllets now travel in straight lines using movement vectors

--- core/src/com/ptd/csis484/Bullet.java
+++ core/src/com/ptd/csis484/Bullet.java
@@ -12,15 +12,31 @@ import com.badlogic.gdx.math.Vector2;
 public class Bullet {
     int damage;
     Enemy target;
-    Vector2 position;
     Tower source;
+    int speed;
+
+    //Vectors to handle bullet motion
+    //Unused vectors are staying incase we change the way we handle movement
+    Vector2 position = new Vector2();
+    Vector2 direction = new Vector2();
+    Vector2 velocity = new Vector2();
+    Vector2 targetPosition = new Vector2();
+    Vector2 movement = new Vector2();
+
 
     public Bullet(int dam, Enemy target, Tower source){
         this.source = source;
         this.damage = source.damage;
         this.target = target;
 
+        //10 is the default speed for now
+        this.speed = 10;
+
+        //Updating our movement vectors
         this.position = new Vector2(source.position.x, source.position.y);
+        this.targetPosition = new Vector2(target.position.x, target.position.y);
+        direction.set(targetPosition).sub(position).nor();
+
     }
 
     //Updates the bullets target to it's source's current target
@@ -35,15 +51,11 @@ public class Bullet {
     }
 
     public void update(float delta) {
-        for (int i = 0; i < 10; i++) {
-            if (target.position.x > position.x)
-                position.x++;
-            if (target.position.x < position.x)
-                position.x--;
-            if (target.position.y > position.y)
-                position.y++;
-            if (target.position.y < position.y)
-                position.y--;
+        //Moves the bullet in the proper direction a number of times equal to it's speed
+        //Looks better than just jumping ahead that many paces and helps with actually hitting
+        //an enemy.
+        for(int i = 0; i < speed; i++) {
+            position.add(direction);
         }
     }
 }
